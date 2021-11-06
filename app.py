3a7d27125c6d97729ad0cac42008cddd1b995b64
ShantingLiu <liushanting@gmail.com>
2021-11-06 18:15:10 -0400

    Hooked up HTML with Flask properly

--- app.py
+++ app.py
@@ -183,5 +183,5 @@ def settings():
 if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "8206")),
+        port=int(os.getenv("PORT", "8208")),
     )
