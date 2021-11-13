5dcf231dc1a8ad377c40a0d6e3d081b36df61f60
amrita07nair <amrita07nair@gmail.com>
2021-11-13 14:53:30 -0500

    added in a comma

--- app.py
+++ app.py
@@ -197,5 +197,5 @@ if __name__ == "__main__":
     app.run(
         #debug=True
         host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "8000"))
+        port=int(os.getenv("PORT", "8000")),
     )
