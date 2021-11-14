09509099f47329f9c29a7a72f939c4de3ce8d0a6
amrita07nair <amrita07nair@gmail.com>
2021-11-14 14:55:15 -0500

    oops

--- app.py
+++ app.py
@@ -207,6 +207,6 @@ def getCompoundName(instr_name, instr_type):
 
 if __name__ == "__main__":
     app.run(
-        debug = True
-        #host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8229")), debug=True
+        #debug = True
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8229")), debug=True
     )
