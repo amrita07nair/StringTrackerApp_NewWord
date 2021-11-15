17f310edd01002f5840ad5002fde4a74bec0a316
amrita07nair <amrita07nair@gmail.com>
2021-11-14 20:19:33 -0500

    minor alteration really quick. test reset db

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def signup_post():
     """
     username = flask.request.form.get("username")
     password = "password"
-    user = User.query.filter_by(username=username, password=password).first()
+    user = User.query.filter_by(username=username).first()
     if user:
          return flask.redirect(flask.url_for("login"))
     else:
@@ -133,7 +133,7 @@ def login_post():
     """
     username = flask.request.form.get("username")
     password = "password"
-    user = User.query.filter_by(username=username, password = password).first()
+    user = User.query.filter_by(username=username).first()
     if user:
         login_user(user)
         return flask.redirect(flask.url_for("home"))
