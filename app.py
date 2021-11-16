f81b3cdd4dfd98d263822e5a67f1ce448b35dd8f
ShantingLiu <liushanting@gmail.com>
2021-11-16 14:48:39 -0500

    Merged Amrita's changes on Login

--- app.py
+++ app.py
@@ -20,23 +20,22 @@ from flask_login import (
     login_required,
 )
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv(find_dotenv())
 
+
 app = flask.Flask(__name__, static_folder="./build/static")
 # Point SQLAlchemy to your Heroku database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL1")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 # Gets rid of a warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "I am a secret key"
-uri = os.getenv("DATABASE_URL1")
+uri = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 # first connect Heroku Postgres to SQLAlchemy
 # https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
-
-
-
 
 
 class User(UserMixin, db.Model):
@@ -45,23 +44,73 @@ class User(UserMixin, db.Model):
     """
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password  = db.Column(db.String(80))
+    email = db.Column(db.String(1000), unique=True, nullable=False)
+    username = db.Column(db.String(1000), unique=True, nullable=False)
+    password = db.Column(db.String(1000))
+    instruments = db.relationship("Instruments", backref="user", lazy=True)
+    current_instr_id = db.Column(db.Integer, nullable=True)
+    str_lifespans = db.relationship("Stringlifespans", backref="user", lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = generate_password_hash(password)
+
     def __repr__(self):
         """
         Determines what happens when we print an instance of the class
         """
-        return f"<User {self.username}, {self.password}>"
+        return f"<User {self.email}, {self.username}>"
+
+    def get_email(self):
+        return self.email
 
     def get_username(self):
         """
         Getter for username attribute
         """
         return self.username
-    
+
     def get_password(self):
 
         return self.password
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Instruments(db.Model):
+    # TODO: Should instr_id be a compound, like Type:Name, or just an int?
+    instr_id = db.Column(db.Integer, primary_key=True)
+    compound_name = db.Column(db.String(240), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    strings = db.relationship("Strings", backref="user", lazy=True)
+    instr_name = db.Column(db.String(120), nullable=False)
+    instr_type = db.Column(db.String(120), nullable=False)
+
+
+class Strings(db.Model):
+    str_id = db.Column(db.Integer, primary_key=True)
+    instr_id = db.Column(
+        db.Integer, db.ForeignKey("instruments.instr_id"), nullable=False
+    )
+    str_name = db.Column(db.String(120), nullable=False)
+    str_cost = db.Column(db.Integer, nullable=False)
+
+
+class Stringlifespans(db.Model):
+    str_lifespan_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    # string_lifespans is a JSON object stored as a string
+    """
+    Ex:
+        - {
+            "Guitar A - String B": [80, 90], 
+            "Guitar B - String C": [100, 120, 130],
+            }
+    """
+    string_lifespans = db.Column(db.String(65535), nullable=False)
+
 
 db.create_all()
 login_manager = LoginManager()
@@ -93,6 +142,7 @@ def logout():
     logout_user()
     return flask.redirect(flask.url_for("login"))
 
+
 @app.route("/signup")
 def signup():
     """
@@ -106,13 +156,14 @@ def signup_post():
     """
     Handler for signup form data
     """
+    email = flask.request.form.get("email")
     username = flask.request.form.get("username")
     password = flask.request.form.get("password")
     user = User.query.filter_by(username=username).first()
     if user:
-         return flask.redirect(flask.url_for("login"))
+        return flask.redirect(flask.url_for("login"))
     else:
-        user = User(username=username, password = password)
+        user = User(email=email, username=username, password=password)
         db.session.add(user)
         db.session.commit()
         return flask.redirect(flask.url_for("login"))
@@ -131,14 +182,12 @@ def login_post():
     """
     Handler for login form data
     """
-    username = flask.request.form.get("username")
+    email = flask.request.form.get("email")
     password = flask.request.form.get("password")
-    user = User.query.filter_by(username=username).first()
-    if user:
-        if user.password == password:
-            login_user(user)
-            return flask.redirect(flask.url_for("home"))
-
+    user = User.query.filter_by(email=email).first()
+    if user and user.verify_password(password):
+        login_user(user)
+        return flask.redirect(flask.url_for("home"))
     return flask.render_template("login.html")
 
 
@@ -171,6 +220,7 @@ def database():
     # TODO: add code here
     return flask.render_template("database.html")
 
+
 """
 @app.route("/database", methods=["POST"])
 @login_required
@@ -193,6 +243,7 @@ def database_post():
     return flask.render_template("database.html")
 """
 
+
 @app.route("/analytics")
 @login_required
 def analytics():
@@ -213,10 +264,12 @@ def getCompoundName(instr_name, instr_type):
 
 if __name__ == "__main__":
     app.run(
-        #debug = True
-        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8229")), debug=True
+        # debug = True
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "8229")),
+        debug=True,
     )
-#up til here username and routing works. time to implement password from here. HTML hasnt broken anything
-#adding password to db.columns and seeing if they breakes anything
-#added in db column for password and hardcoded filler password to test if db will accept new column
-#basic password stuff working!!!!!!!! now polish and add flask.flask or jsonify :)
+# up til here username and routing works. time to implement password from here. HTML hasnt broken anything
+# adding password to db.columns and seeing if they breakes anything
+# added in db column for password and hardcoded filler password to test if db will accept new column
+# basic password stuff working!!!!!!!! now polish and add flask.flask or jsonify :)
