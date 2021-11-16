7037831b70c8fc0983c22e44479b01fdf659abc1
amrita07nair <amrita07nair@gmail.com>
2021-11-15 21:22:24 -0500

    changing password length in db

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80))
     username = db.Column(db.String(80))
-    password  = db.Column(db.String(80))
+    password  = db.Column(db.String(1000))
     
     def __repr__(self):
         """
